e64a48a9f8d05adf7dbceabe87ef7a81abe43b41
AndrejPavic <62964361+AndrejPavic@users.noreply.github.com>
2024-03-06 17:15:09 +0100

    Create PR2.c

---
+++ Primjeri/PR2.c
@@ -0,0 +1,16 @@
+//Ispis brojeva od 1 do 10:
+
+#include <stdio.h>
+
+int main() {
+    int brojac = 1;
+
+    while (brojac <= 10) {
+        printf("%d ", brojac);
+        brojac++;
+    }
+
+    printf("\n");
+
+    return 0;
+}
